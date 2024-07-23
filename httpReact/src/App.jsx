e1042daed31804d180bd9c2d00ce5aea98e78f5c
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2024-07-23 20:38:15 -0300

    POST

--- httpReact/src/App.jsx
+++ httpReact/src/App.jsx
@@ -6,6 +6,11 @@ function App() {
   const [products, setProducts] = useState([])
   const url = "http://localhost:3000/products"
 
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+    //resgatando dados
+
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(url);
@@ -18,6 +23,26 @@ function App() {
 
   },[])
 
+  // add de produtos
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    }
+   const res = await fetch(url,  {
+    method: "POST",
+    headers: {
+      "Content-Type": "aplication/json"
+    },
+    body: JSON.stringify(product),
+   })
+   
+  }
+
+
   return (
     <div className='App'>
     <h1>Lista de Produtos</h1>
@@ -28,6 +53,19 @@ function App() {
         </li>
       ))}
     </ul>
+    <div className="add-product">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome:
+          <input type="text" value={name} name ="name" onChange={(e)=> setName(e.target.value)}/>
+        </label>
+        <label>
+          Preço:
+          <input type="text" value={price} name ="price" onChange={(e)=> setPrice(e.target.value)}/>
+        </label>
+        <input type="submit" value="criar" />
+      </form>
+    </div>
     </div>
   )
 }
